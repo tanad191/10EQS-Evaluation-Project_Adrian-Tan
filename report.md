d476e22da91f4953d469e4b06e26cb1942aa9867
Adrian Tan <tan.191@buckeyemail.osu.edu>
2025-01-05 23:34:46 -0500

    External source change
    Changed the external source to in2013dollars.com, as well as enabling webscraping of this source and updating statistics to measure.

--- report.md
+++ report.md
@@ -1,28 +1,24 @@
 # Overview
-This study serves as a comparison between pricing data obtained from our local coffee shop and global pricing data for goods the shop would require. The business insight to be gleaned from this therefore concerns whether the earnings made per year by the shop would be enough to recoup the expenses required to have coffee and tea delivered, shipped, and/or imported to the shop to serve as inventory stock.
+This study serves as a comparison between pricing data obtained from our local coffee shop and average pricing data for restaurant-sold coffee in the US. The business insight to be gleaned from this therefore concerns how much the earnings made per year by the shop deviates from the nationwide average per year, which can be used to determine if prices are too high or too low compared to this average as a factor into critical business decisions.
 # Data quality issues and wrangling
 The products.csv file used for this study is stored in the /data directory of this project, and was read into the Python script using Pandas read_csv() function. 
 # Data summary
 
 # External data integration
-Our external source is FAOstats free agricultural production dataset. This will allow us to obtain productivity for both coffee and tea in USD per metric ton, which we can convert to USD per pound. We will select data for just one year, in this case 2022.
-Webscraping attempts with BeautifulSoup resulted in the HTML showing no tables at all due to the tables being represented as widgets for the sake of user-input filtering, so the CSV format of the data had to be manually downloaded. The CSVs were stored in the /data directory of this project, and as with the products.csv file they were read into the Python script using Pandas read_csv() function.
-The price per year for both coffee and tea is assumed to be static from year to year, and that we purchase all coffee and tea products at the same price, which is the global price per year.
-- Price URL: https://www.fao.org/faostat/en/#data/QV?regions=5000&elements=57&items=656,667&years=2022&output_type=table&file_type=csv&submit=true
-- Quantity URL: https://www.fao.org/faostat/en/#data/QV?regions=5000&elements=57&items=656,667&years=2022&output_type=table&file_type=csv&submit=true
+Our external source is in2013dollars.coms free dataset prices of <i>beverage materials including coffee and tea</i> from 1997 to 2024. This will allow us to obtain average annually coffee price in USD per pound. We will select data for the most recent year, in this case 2024 (the most recent full year as of this study).
+Webscraping was done with BeautifulSoup, which enabled extraction of the HTML table and refinement of the external data to remove HTML formatting and preserve the numerical values for the year, Value (both normal and adjusted for inflation), and Inflation Rate.
+The price per year for coffee is assumed to be static from year to year.
+Source URL: https://www.in2013dollars.com/inflation/coffee-prices-by-year-and-adjust-for-inflation/
 # Business insights
-For our economic insight, we compared the total per-pound price of coffee, tea, etc. for this store per year with the global price per pound per year. This allowed us to determine the difference between the yearly earnings by our shop and the global supply price per year, which could indicate whether a profit is being made by our shop based on the amount of goods it needs per year.
+For our economic insight, we compared the total per-pound price of both coffee and tea for this store per year with the average price per pound per year. This allowed us to determine the difference between the yearly earnings by coffee sold at our shop and the average price per year for both types, which could indicate whether we are above or below the national average and to what extent. This can be important to know when it comes to determining how to increase customer inflow; if we are above average, customers may not be willing to buy from us as often as with other stores.
 ## Inventory earnings per year
 For calculation simplicity, the restock threshold is assumed to indicate how much of each product will be purchased per month, and we will also assume that every single unit we receive is sold before the end of the year.
 To obtain the pound-weight for each item, the numerical quantity per unit is extracted from each product name (column "product_name"). If this number is absent, the product is assumed to be sold at a rate of 1 pound per package.
 Should quantity per unit be present in the product name, it will be converted into pounds per unit; the program checks what unit the item is sold in, and if the amount of an item per package is in pounds then that amount will be used in subsequent calculations as-is. If the item is sold in ounces, the number of ounces per package is divided by 16 to obtain the pounds per package. For tea bags, a pound of tea leaves is assumed to be equivalent to 200 bags, so the amount of bags is divided by 200 to obtain the pound weight. Once the pound-weights per unit for all items are obtained, we multiply the price for each item by its restock threshold to get price per monthly restock, and the pound-weight for each item by its restock threshold to get quantity per monthly restock.
-We then extract the sum of all prices per monthly restock for items in the "coffee" category and multiply this by twelve months to get the total supply price per year for all coffee items. Then we do the same with all items in the "tea" category. Thus, we have:
-- Coffee earnings per year: $16,848.60
-- Tea earnings per year: $13,965.96
-## Restock expenses per year
-From the FAOSTAT data CSVs, we extract QV and multiply it by 1000 for each item type (coffee and tea) to obtain the total global production value (which is in thousands of USD), and extract QCL and multiply it by 2204.62 to obtain global production amount (converting from metric tons to pounds). Dividing global production value by global production amount for each item type gives us the price per pound for that item, assuming all varieties of that item cost the same to buy regardless of where they came from.
-We then extract the sum of all weights per monthly restock for items in each category and multiply this by both twelve months and to get the total supply price per year for all items of that category. Thus, we have:
-- Coffee expenses per year: $924.41
-- Tea expenses per year: $491.27
+We then extract the sum of all prices per monthly restock for items in the "coffee" or "tea" categories and multiply this by twelve months to get the total supply price per year for all coffee and tea items put together. Thus, we have:
+Total local beverage earnings per year: <b>$35,249.76</b>
+## Average coffee price per year
+From the webscraped in2013dollars.com dataset, we extract the average coffee and tea price from the year 2024. We then extract the sum of all weights per monthly restock for items in each category and multiply this by both twelve months and to get the total supply price per year for all items of that category. Thus, we have:
+Total national average price per year: <b>$56,048.64</b>
 # Future recommendations
 
--- src/analysis.py
+++ src/analysis.py
@@ -1,8 +1,10 @@
+from bs4 import BeautifulSoup
 import markdown
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import requests
 import seaborn as sns
 from utils import extract_column_from_header
 
@@ -40,9 +42,8 @@ tea_list = ['tea', 'matcha', 'chai']
 tea_pattern = '|'.join(tea_list)
 inventory_df.loc[inventory_df['product_name'].str.contains(tea_pattern, case=False), 'category'] = 'tea'
 
-# For our economic insight, we will be comparing the total per-pound price of coffee, tea, etc. for this store per year with the global price per pound per year.
-# This will allow us to determine the difference between the yearly earnings by our shop and the global expenses per year,
-# which could indicate whether a profit is being made by our shop based on the amount of goods it needs per year.
+# For our economic insight, we will be comparing the total per-pound price of beverage materials including coffee and tea for this store per year with the average price per pound per year for both beverage types.
+# This will allow us to determine the deviation between the yearly earnings by our shop and the average expenses per year.
 # Assume that the restock threshold indicates how much of each product will be purchased per month.
 
 # Extract weight per unit from each product_name. If the unit is absent, assume the product is sold in batches of 1 pound.
@@ -76,38 +77,95 @@ for index, row in inventory_df.iterrows():
 # Output the DataFrame for checking purposes
 # print(inventory_df)
 
-# Extract the sum of all price_per_restock where category = coffee, then multiply by 12 months to get the total cost per year. Then do the same with category = tea.
-coffee_earnings = inventory_df[inventory_df['category']=='coffee']['price_per_restock'].sum() * 12
-tea_earnings = inventory_df[inventory_df['category']=='tea']['price_per_restock'].sum() * 12
+# Extract the sum of all price_per_restock, then multiply by 12 months to get the total cost per year.
+#coffee_earnings = inventory_df[inventory_df['category']=='coffee']['price_per_restock'].sum()
+#tea_earnings = inventory_df[inventory_df['category']=='coffee']['price_per_restock'].sum()
+#beverage_earnings = (coffee_earnings + tea_earnings) * 12
+bvg_earnings_per_month = inventory_df['price_per_restock'].sum()
+beverage_earnings = bvg_earnings_per_month * 12
 
-print('Coffee earnings per year: ${:,.2f}'.format(coffee_earnings))
-print('Tea earnings per year: ${:,.2f}'.format(tea_earnings))
+print('Local beverage earnings per year: ${:,.2f}'.format(beverage_earnings))
 
-# Now we obtain the global prices per pound (the supply) and extract the coffee and tea prices.
+# Now we obtain the global prices per pound (the supply) and extract the coffee prices.
 
-# Read the CSV file from the data folder
-file_path_qv = os.path.join(script_dir, '../data/FAOSTAT_data_en_QV_2022.csv')
-file_path_qcl = os.path.join(script_dir, '../data/FAOSTAT_data_en_QCL_2022.csv')
-qv_df = pd.read_csv(file_path_qv)
-qcl_df = pd.read_csv(file_path_qcl)
+# First, obtain the average prices per pound and extract the coffee prices.
+# We're using in2013dollars.com's historical chart data for coffee prices per pound by year.
+static_url = "https://www.in2013dollars.com/Beverage-materials-including-coffee-and-tea/price-inflation"
+in2013dollars_data  = requests.get(static_url).text
 
-# Now, get the global production in USD/metric ton for coffee and tea, and for each, convert it to USD/pound.
-coffee_production_price = float(qv_df.loc[qv_df['Item'].str.contains('coffee', case=False), 'Value']) * 1000
-coffee_production_quantity = float(qcl_df.loc[qcl_df['Item'].str.contains('coffee', case=False), 'Value']) * 2204.62
+# Alternative data source to check if webscraping is possible: https://www.in2013dollars.com/Other-beverage-materials-including-tea/price-inflation
 
-tea_production_price = float(qv_df.loc[qv_df['Item'].str.contains('tea', case=False), 'Value']) * 1000
-tea_production_quantity = float(qcl_df.loc[qcl_df['Item'].str.contains('tea', case=False), 'Value']) * 2204.62
+# Use BeautifulSoup() to create a BeautifulSoup object from a response text content, then verify if the parse ran correctly
+in2013dollars_soup = BeautifulSoup(in2013dollars_data, 'html.parser')
+print(in2013dollars_soup.title) # To verify whether this works
 
-coffee_production_per_pound = coffee_production_price/coffee_production_quantity
-tea_production_per_pound = tea_production_price/tea_production_quantity
+# Use the find_all function in the BeautifulSoup object with the element type of 'table', then assign the result to a list called 'html_tables'
+html_tables = in2013dollars_soup.find_all('table')
+in2013dollars_table = html_tables[0]
+print(in2013dollars_table.title)
 
-# Extract the sum of all weight_per_restock where category = coffee and multiply this by coffee_production_per_pound to get the expenses.
-# Then do the same with category = tea.
-coffee_expenses = inventory_df[inventory_df['category']=='coffee']['weight_per_restock'].sum() * 12 * coffee_production_per_pound
-tea_expenses = inventory_df[inventory_df['category']=='tea']['weight_per_restock'].sum() * 12 * tea_production_per_pound
+column_names = []
 
-print('Coffee expenses per year: ${:,.2f}'.format(coffee_expenses))
-print('Tea expenses per year: ${:,.2f}'.format(tea_expenses))
+# Apply the find_all() function with 'th' element on products_table
+# Iterate each th element and apply the provided extract_column_from_header() to get a column name
+# Append the Non-empty column name (`if name is not None and len(name) > 0`) into a list called column_names
+column_headers = in2013dollars_table.find_all('th')
+for row in column_headers:
+    name = extract_column_from_header(row)
+    if name is not None and len(name) > 0:
+        column_names.append(name)
+
+# print(column_names)
+
+# We will create an empty dictionary with keys from the extracted column names. We can then convert this into a Pandas dataframe
+in2013dollars_dict= dict.fromkeys(column_names)
+
+# Initialize the in2013dollars_dict with each value to be an empty list
+in2013dollars_dict['Year'] = []
+in2013dollars_dict['USD Value'] = []
+in2013dollars_dict['Inflation Rate'] = []
+
+for rows in in2013dollars_table.find_all("tr"):
+    if rows.th:
+        flag = False
+    else:
+        flag = True
+    row=rows.find_all('td')
+    # print(row)
+    
+    if flag:
+        year=row[0].get_text()
+        # print(crop)
+        in2013dollars_dict['Year'].append(int(year))
+        
+        value=row[1].get_text()
+        # print(global_gpv)
+        value_num = value.strip('$')
+        in2013dollars_dict['USD Value'].append(float(value_num))
+        
+        inflation_rate = row[2].get_text()
+        # print(global_prod)
+        inflation_rate_num = inflation_rate.replace('%','').replace('*','')
+        if any(chr.isdigit() for chr in inflation_rate_num):
+            in2013dollars_dict['Inflation Rate'].append(float(inflation_rate_num))
+        else: #To cover the null value for this column in 1997
+            in2013dollars_dict['Inflation Rate'].append(0.0)
+
+in2013dollars_df= pd.DataFrame({ key:pd.Series(value) for key, value in in2013dollars_dict.items() })
+
+print(in2013dollars_df)
+
+# Now, extract the average adjusted price in USD/pound for the year 2024.
+beverage_average_price = float(in2013dollars_df.loc[in2013dollars_df['Year']==2024, 'USD Value'])
+
+# Extract the sum of all weight_per_restock from inventory_df, and multiply this by 12 months and then by beverage_average_price to get the annual expenses.
+# coffee_quantity = inventory_df[inventory_df['category']=='coffee']['weight_per_restock'].sum()
+# tea_quantity = inventory_df[inventory_df['category']=='coffee']['weight_per_restock'].sum()
+# beverage_expenses = (coffee_quantity + tea_quantity) * 12 * beverage_average_price
+beverage_quantity = inventory_df['weight_per_restock'].sum()
+beverage_average = beverage_quantity * 12 * beverage_average_price
+
+print('Total beverage price per year: ${:,.2f}'.format(beverage_average))
 
 # Finally, print the report.md file. Remove any previous report.md file first to clear the way for generating a new one.
 if os.path.exists('report.md'):
@@ -115,31 +173,27 @@ if os.path.exists('report.md'):
   
 with open('report.md', 'a+') as f:
     f.write('# Overview\n')
-    f.write('This study serves as a comparison between pricing data obtained from our local coffee shop and global pricing data for goods the shop would require. The business insight to be gleaned from this therefore concerns whether the earnings made per year by the shop would be enough to recoup the expenses required to have coffee and tea delivered, shipped, and/or imported to the shop to serve as inventory stock.\n')
+    f.write('This study serves as a comparison between pricing data obtained from our local coffee shop and average pricing data for restaurant-sold coffee in the US. The business insight to be gleaned from this therefore concerns how much the earnings made per year by the shop deviates from the nationwide average per year, which can be used to determine if prices are too high or too low compared to this average as a factor into critical business decisions.\n')
     f.write('# Data quality issues and wrangling\n')
     f.write('The products.csv file used for this study is stored in the /data directory of this project, and was read into the Python script using Pandas'' read_csv() function. \n')
     f.write('# Data summary\n')
     f.write('\n')
     f.write('# External data integration\n')
-    f.write('Our external source is FAOstat''s free agricultural production dataset. This will allow us to obtain productivity for both coffee and tea in USD per metric ton, which we can convert to USD per pound. We will select data for just one year, in this case 2022.\n')
-    f.write('Webscraping attempts with BeautifulSoup resulted in the HTML showing no tables at all due to the tables being represented as widgets for the sake of user-input filtering, so the CSV format of the data had to be manually downloaded. The CSVs were stored in the /data directory of this project, and as with the products.csv file they were read into the Python script using Pandas'' read_csv() function.\n')
-    f.write('The price per year for both coffee and tea is assumed to be static from year to year, and that we purchase all coffee and tea products at the same price, which is the global price per year.\n')
-    f.write('- Price URL: https://www.fao.org/faostat/en/#data/QV?regions=5000&elements=57&items=656,667&years=2022&output_type=table&file_type=csv&submit=true\n')
-    f.write('- Quantity URL: https://www.fao.org/faostat/en/#data/QV?regions=5000&elements=57&items=656,667&years=2022&output_type=table&file_type=csv&submit=true\n')
+    f.write('Our external source is in2013dollars.com''s free dataset prices of <i>beverage materials including coffee and tea</i> from 1997 to 2024. This will allow us to obtain average annually coffee price in USD per pound. We will select data for the most recent year, in this case 2024 (the most recent full year as of this study).\n')
+    f.write('Webscraping was done with BeautifulSoup, which enabled extraction of the HTML table and refinement of the external data to remove HTML formatting and preserve the numerical values for the year, Value (both normal and adjusted for inflation), and Inflation Rate.\n')
+    f.write('The price per year for coffee is assumed to be static from year to year.\n')
+    f.write('Source URL: https://www.in2013dollars.com/inflation/coffee-prices-by-year-and-adjust-for-inflation/\n')
     f.write('# Business insights\n')
-    f.write('For our economic insight, we compared the total per-pound price of coffee, tea, etc. for this store per year with the global price per pound per year. This allowed us to determine the difference between the yearly earnings by our shop and the global supply price per year, which could indicate whether a profit is being made by our shop based on the amount of goods it needs per year.\n')
+    f.write('For our economic insight, we compared the total per-pound price of both coffee and tea for this store per year with the average price per pound per year. This allowed us to determine the difference between the yearly earnings by coffee sold at our shop and the average price per year for both types, which could indicate whether we are above or below the national average and to what extent. This can be important to know when it comes to determining how to increase customer inflow; if we are above average, customers may not be willing to buy from us as often as with other stores.\n')
     f.write('## Inventory earnings per year\n')
     f.write('For calculation simplicity, the restock threshold is assumed to indicate how much of each product will be purchased per month, and we will also assume that every single unit we receive is sold before the end of the year.\n')
     f.write('To obtain the pound-weight for each item, the numerical quantity per unit is extracted from each product name (column "product_name"). If this number is absent, the product is assumed to be sold at a rate of 1 pound per package.\n')
     f.write('Should quantity per unit be present in the product name, it will be converted into pounds per unit; the program checks what unit the item is sold in, and if the amount of an item per package is in pounds then that amount will be used in subsequent calculations as-is. If the item is sold in ounces, the number of ounces per package is divided by 16 to obtain the pounds per package. For tea bags, a pound of tea leaves is assumed to be equivalent to 200 bags, so the amount of bags is divided by 200 to obtain the pound weight. Once the pound-weights per unit for all items are obtained, we multiply the price for each item by its restock threshold to get price per monthly restock, and the pound-weight for each item by its restock threshold to get quantity per monthly restock.\n')
-    f.write('We then extract the sum of all prices per monthly restock for items in the "coffee" category and multiply this by twelve months to get the total supply price per year for all coffee items. Then we do the same with all items in the "tea" category. Thus, we have:\n')
-    f.write('- Coffee earnings per year: <b>${:,.2f}</b>\n'.format(coffee_earnings))
-    f.write('- Tea earnings per year: <b>${:,.2f}</b>\n'.format(tea_earnings))
-    f.write('## Restock expenses per year\n')
-    f.write('From the FAOSTAT data CSVs, we extract QV and multiply it by 1000 for each item type (coffee and tea) to obtain the total global production value (which is in thousands of USD), and extract QCL and multiply it by 2204.62 to obtain global production amount (converting from metric tons to pounds). Dividing global production value by global production amount for each item type gives us the price per pound for that item, assuming all varieties of that item cost the same to buy regardless of where they came from.\n')
-    f.write('We then extract the sum of all weights per monthly restock for items in each category and multiply this by both twelve months and to get the total supply price per year for all items of that category. Thus, we have:\n')
-    f.write('- Coffee expenses per year: <b>${:,.2f}</b>\n'.format(coffee_expenses))
-    f.write('- Tea expenses per year: <b>${:,.2f}</b>\n'.format(tea_expenses))
+    f.write('We then extract the sum of all prices per monthly restock for items in the "coffee" or "tea" categories and multiply this by twelve months to get the total supply price per year for all coffee and tea items put together. Thus, we have:\n')
+    f.write('Total local beverage earnings per year: <b>${:,.2f}</b>\n'.format(beverage_earnings))
+    f.write('## Average coffee price per year\n')
+    f.write('From the webscraped in2013dollars.com dataset, we extract the average coffee and tea price from the year 2024. We then extract the sum of all weights per monthly restock for items in each category and multiply this by both twelve months and to get the total supply price per year for all items of that category. Thus, we have:\n')
+    f.write('Total national average price per year: <b>${:,.2f}</b>\n'.format(beverage_average))
     f.write('# Future recommendations\n')
     f.write('\n')
     # markdown.markdownFromFile(input=f, output=dir+'/out.html')
